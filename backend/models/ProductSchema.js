c8a788efe7ec583f817cb84fbb570178f6937f52
matand380 <matand94@cs.colman.ac.il>
2023-06-21 20:49:08 +0300

    backend update

--- backend/models/ProductSchema.js
+++ backend/models/ProductSchema.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     name: {type: String, required: true},
     description: {type: String},
     price: {type: Number, required: false},
--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const router = express.Router();
 const productController = require('../controllers/ProductController');
+const router = express.Router();
 
 router.get('/',productController.getAllProducts);
 router.post('/', productController.createProduct);
 router.get('/:id',productController.getProductById);
 router.patch('/',productController.updateProduct)
 router.delete('/', productController.deleteProduct);
+// router.post('/filter', productController.filterProduct)
 
 
 module.exports = router;
