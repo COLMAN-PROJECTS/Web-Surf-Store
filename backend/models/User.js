be6b6672229e488da22c23192554169bae5442a8
DanDavidovich <davidovich.dan@gmail.com>
2023-06-22 09:29:16 +0300

    User model

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const User = new mongoose.Schema({
+    _id:String,
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    phone:{
+        type:String,
+    },
+    role:{
+        type:String,
+        required:true,
+    }
+});
+
+module.exports = mongoose.model("User", User);
