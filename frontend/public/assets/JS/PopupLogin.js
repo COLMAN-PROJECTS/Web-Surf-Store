1b23bba6a14ae2b1529abdaaac377dca9db78e2d
DanDavidovich <davidovich.dan@gmail.com>
2023-06-23 15:43:02 +0300

    using jquery

--- frontend/public/assets/JS/PopupLogin.js
+++ frontend/public/assets/JS/PopupLogin.js
@@ -6,29 +6,13 @@ $(document).ready(function() {
         <head>
           <title>Login</title>
           <link rel="stylesheet" type="text/css" href="/frontend/public/assets/CSS/login.css">
-          <style>
-            body {
-              display: flex;
-              align-items: center;
-              justify-content: center;
-              height: 100vh;
-              margin: 0;
-            }
-
-            .popup-container {
-              width: 400px;
-              height: 300px;
-              background-color: #fff;
-              padding: 20px;
-              box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-              text-align: center;
-            }
+          <style class="body,popup-container" >
           </style>
         </head>
         <body>
           <div class="popup-container">
             <h2 class="popup-title">Login</h2>
-            <form id="login-form" class="popup-form" onsubmit="login(event)">
+            <form id="login-form" class="popup-form">
               <label for="email">Email:</label>
               <input type="email" id="email" name="email" required>
               <br>
@@ -39,41 +23,42 @@ $(document).ready(function() {
             </form>
           </div>
         </body>
-      </html>
-    `;
+      </html>`;
 
         popup.document.write(popupContent);
         popup.document.close();
 
-        popup.document.getElementById("Enter").addEventListener("click", function(event) {
+        $(popup.document).find("#Enter").click(function(event) {
             event.preventDefault();
+            var enteredEmail = $(popup.document).find("#email").val();
+            var enteredPassword = $(popup.document).find("#password").val();
 
-            var enteredEmail = popup.document.getElementById("email").value;
-            var enteredPassword = popup.document.getElementById("password").value;
-
-            fetch("./DB/UserSeed.json")
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error("Failed to fetch user data.");
-                    }
-                    return response.json();
-                })
-                .then(userData => {
-                    var loggedInUser = userData.find(user => user.email === enteredEmail && user.password === enteredPassword);
+            $.ajax({
+                url: "./DB/UserSeed.json",
+                dataType: "json",
+                type: "POST",
+                data: {
+                    email: enteredEmail,
+                    password: enteredPassword
+                },
+                success: function(userData) {
+                    var loggedInUser = userData.find(user => user.email === enteredEmail
+                                                    && user.password === enteredPassword);
 
                     if (loggedInUser) {
                         alert("Login successful!");
                         popup.close();
                     } else {
-                        popup.alert("Login failed!,wrong password or email!");
+                        popup.alert("Login failed! Wrong password or email!");
                     }
 
-                    popup.document.getElementById("email").value = "";
-                    popup.document.getElementById("password").value = "";
-                })
-                .catch(error => {
-                    alert("Error fetching user data: " + error.message);
-                });
+                    $(popup.document).find("#email").val("");
+                    $(popup.document).find("#password").val("");
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    alert("Error fetching user data: " + errorThrown);
+                }
+            });
         });
     });
 });
