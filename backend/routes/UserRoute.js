baa5322bbe7ac53bc3fa9981898756d3d1023c06
matand380 <matand94@cs.colman.ac.il>
2023-06-26 21:04:03 +0300

    added isAdmin middleware

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const userController = require('../controllers/UserController');
+const isAdmin = require('../middlewares/isAdminMiddleWare');
 const router = express.Router();
 
-router.get("/", userController.getAllUsers);
-router.get("/:id", userController.getUserById);
-router.get("/email/:email", userController.getUserByEmail);
-router.patch("/update", userController.updateUser);
-router.delete("/delete", userController.deleteUser);
+router.get("/",isAdmin, userController.getAllUsers);
+router.get("/:id",isAdmin, userController.getUserById);
+router.get("/email/:email",isAdmin, userController.getUserByEmail);
+router.patch("/update",isAdmin, userController.updateUser);
+router.delete("/delete",isAdmin, userController.deleteUser);
 
 module.exports = router;
