5aaaa25fc38d3b33799076d311751eb9aeb318ba
DanDavidovich <davidovich.dan@gmail.com>
2023-06-26 12:20:54 +0300

    test push

--- frontend/public/assets/JS/weather.js
+++ frontend/public/assets/JS/weather.js
@@ -12,7 +12,6 @@ async function getWeatherData() {
   // Display loading message
   displayLoadingMessage();
 
-
   // Get the user's current location
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(async position => {
