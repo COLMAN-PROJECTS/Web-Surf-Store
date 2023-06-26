cc0cd36ea55956d7170616ff849634d7836e47db
matand380 <matand94@cs.colman.ac.il>
2023-06-27 00:00:08 +0300

    added BeachInfoSchema

---
+++ backend/models/BeachInfoSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const BeachInfoSchema = new Schema({
+    spot: {type: String, required: true},
+    lat: {type: Number, required: true},
+    long: {type: Number, required: true},
+    description: {type: String, required: true},
+    video: {type: String, required: true}
+})
+
+const BeachInfo = mongoose.model('BeachInfo', BeachInfoSchema);
+
+module.exports = BeachInfo;
