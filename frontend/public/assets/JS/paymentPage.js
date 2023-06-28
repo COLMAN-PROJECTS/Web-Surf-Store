e9c195d4653cf5ef674ba8e7a44b0e6a9504fcf6
Eviatar109 <Eviatar109@gmail.com>
2023-06-28 13:00:50 +0300

    organized paymentPage.js

--- frontend/public/assets/JS/paymentPage.js
+++ frontend/public/assets/JS/paymentPage.js
@@ -1,23 +1,12 @@
-window.innerWidth < 768 &&
-[].slice
-  .call(document.querySelectorAll("[data-bss-disabled-mobile]"))
-  .forEach(function (e) {
-    e.classList.remove("animated"),
-      e.removeAttribute("data-bss-hover-animate"),
-      e.removeAttribute("data-aos"),
-      e.removeAttribute("data-bss-parallax-bg"),
-      e.removeAttribute("data-bss-scroll-zoom");
-  }),
-  document.addEventListener("DOMContentLoaded", function () {
-    $(document).ready(function () {
-     initializeImageTitle();
-    });
+$(document).ready(function () {
+  initializeImageTitle();
+});
 
-    function initializeImageTitle() {
-      let imageUrl = 'assets/images/TitleImage/imageTitlePayment.webp';
-      let backgroundImage = 'url(' + imageUrl + ') center / cover';
+function initializeImageTitle() {
+  let imageUrl = 'assets/images/TitleImage/imageTitlePayment.webp';
+  let backgroundImage = 'url(' + imageUrl + ') center / cover';
 
-      $('#titleImage').css('background', backgroundImage);
-      $("#titleH1").text('Checkout Page');
-    }
-  });
+  $('#titleImage').css('background', backgroundImage);
+  $("#titleH1").text('Checkout Page');
+}
+
