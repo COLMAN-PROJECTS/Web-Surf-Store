4100f72b22477f37f5b7e4460d429464e2e7e3a2
Eviatar109 <Eviatar109@gmail.com>
2023-06-28 12:59:53 +0300

    organized managerAreaPage.js

--- frontend/public/assets/JS/managerAreaPage.js
+++ frontend/public/assets/JS/managerAreaPage.js
@@ -1,16 +1,4 @@
-window.innerWidth < 768 &&
-[].slice
-    .call(document.querySelectorAll("[data-bss-disabled-mobile]"))
-    .forEach(function (e) {
-        e.classList.remove("animated"),
-            e.removeAttribute("data-bss-hover-animate"),
-            e.removeAttribute("data-aos"),
-            e.removeAttribute("data-bss-parallax-bg"),
-            e.removeAttribute("data-bss-scroll-zoom");
-    }),
-
-
-    $(document).ready(function () {
+ $(document).ready(function () {
         productButtons();
         initializeImageTitle()
         getDataForTable()
