2f59605f5027bc2d280a2084614ba862d9d83ab4
DanDavidovich <davidovich.dan@gmail.com>
2023-06-29 16:13:55 +0300

    payment and sending orders

--- frontend/public/assets/JS/paymentPage.js
+++ frontend/public/assets/JS/paymentPage.js
@@ -10,3 +10,92 @@ function initializeImageTitle() {
   $("#titleH1").text('Checkout Page');
 }
 
+  function sendOrder(orderData) {
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:3000/orders",
+    data: orderData,
+    success: function (data) {
+      console.log("Order sent successfully");
+        $("#orderCompleted").show();
+      setTimeout(function () {
+        window.location.href = './index.html'
+      }, 5000);
+
+    },
+    error: function (data) {
+      console.log("Error sending order");
+      $("#orderNotCompleted").show();
+      setTimeout(function () {
+        $("#orderNotCompleted").hide();
+      }, 2500);
+    }
+  });
+  }
+  function validateForm() {
+    var userName = $("#userName").val();
+    var cardNumber = $("#creditCardNumber").val();
+    var MM = $("#MM").val();
+    var YY = $("#YY").val();
+    var CVV = $("#CVV").val();
+
+    // Perform basic validation tests
+    if (userName.trim() === "") {
+      console.log("wrong details");
+      $("#orderNotCompleted").show();
+      setTimeout(function () {
+        $("#orderNotCompleted").hide();
+      }, 2500);
+      return false;
+    }
+
+    if (cardNumber.trim() === "") {
+      console.log("wrong details");
+      $("#orderNotCompleted").show();
+      setTimeout(function () {
+        $("#orderNotCompleted").hide();
+      }, 2500);
+      return false;
+    }
+
+    if (MM.trim() === "" || YY.trim() === "") {
+      console.log("wrong details");
+      $("#orderNotCompleted").show();
+      setTimeout(function () {
+        $("#orderNotCompleted").hide();
+      }, 2500);
+      return false;
+    }
+
+    if (CVV.trim() === "") {
+      console.log("wrong details");
+      $("#orderNotCompleted").show();
+      setTimeout(function () {
+        $("#orderNotCompleted").hide();
+      }, 2500);
+      return false;
+    }
+
+    return true;
+  }
+
+
+  $("#confirmBtn").click(function() {
+
+    if (validateForm()) {
+      var paymentDetails = {
+        //get user identification
+        user:localStorage.getItem('user.id'),
+        products: localStorage.getItem("products"),
+        shippingAddress: localStorage.getItem("shippingAddress"),
+        paymentMethod: 'Credit Card',
+        userName: $("#userName").val(),
+        cardNumber: $("#creditCardNumber").val(),
+        expirationMonth: $("#MM").val(),
+        expirationYear: $("#YY").val(),
+        cvv: $("#CVV").val()
+      };
+      console.log(paymentDetails);
+      sendOrder(paymentDetails);
+    }
+  });
