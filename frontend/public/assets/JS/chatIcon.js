ef2e06f23f45719d3e11e3fb5d02fecf3d9b3559
DanDavidovich <davidovich.dan@gmail.com>
2023-06-30 18:29:36 +0300

    description fix

--- frontend/public/assets/JS/chatIcon.js
+++ frontend/public/assets/JS/chatIcon.js
@@ -1,15 +1,17 @@
-$(document).ready(function() {
-  $('#chatIcon').click(function() {
-    $.ajax({
-      url: 'http://localhost:3000/chat',
-      method: 'POST',
-      success: function(response) {
-        console.log('Chat started successfully');
-      },
-      error: function(xhr, status, error) {
-        console.log('Error starting chat:', error);
-        alert('Error starting chat:'+ error);
-      }
-    });
-  });
-});
+//can be removed if the chat button
+
+// $(document).ready(function() {
+//   $('#chatIcon').click(function() {
+//     $.ajax({
+//       url: 'http://localhost:3000/chat',
+//       method: 'POST',
+//       success: function(response) {
+//         console.log('Chat started successfully');
+//       },
+//       error: function(xhr, status, error) {
+//         console.log('Error starting chat:', error);
+//         alert('Error starting chat:'+ error);
+//       }
+//     });
+//   });
+// });
