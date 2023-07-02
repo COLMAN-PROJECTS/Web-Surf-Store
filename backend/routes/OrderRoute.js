f51e64bb94e0f7db26ae3c815fd20471620c60fa
matand380 <matand94@cs.colman.ac.il>
2023-07-02 10:12:22 +0300

    updates

--- backend/routes/OrderRoute.js
+++ backend/routes/OrderRoute.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const orderController = require("../controllers/orderController");
 const isAdmin = require('../middlewares/isAdminMiddleWare');
 
-router.get('/',isAdmin, orderController.getAllOrders);
+router.get('/', orderController.getAllOrders);
 router.post('/', orderController.createOrder);
-router.get('/:id',isAdmin, orderController.getOrderById);
-router.patch('/',isAdmin, orderController.updateOrder)
-router.delete('/',isAdmin, orderController.deleteOrder);
+router.get('/:id', orderController.getOrderById);
+router.patch('/', orderController.updateOrder)
+router.delete('/', orderController.deleteOrder);
 router.post('/filter', orderController.filterOrders)
 router.get('/groupBy/:field', orderController.groupByField);
 
