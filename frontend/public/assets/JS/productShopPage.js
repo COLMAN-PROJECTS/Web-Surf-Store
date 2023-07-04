d6c430730305d20b1babdfc498552c204f781693
Eviatar109 <Eviatar109@gmail.com>
2023-07-05 00:24:13 +0300

    fix no product found

--- frontend/public/assets/JS/productShopPage.js
+++ frontend/public/assets/JS/productShopPage.js
@@ -34,129 +34,128 @@ $(document).ready(function () {
 });
 
 function productCartClick() {
-    let aId = $(this).attr('id');
-    console.log(aId);
-    let productId = aId.split('_')[1];
+  let aId = $(this).attr('id');
+  console.log(aId);
+  let productId = aId.split('_')[1];
 
-    let product = filteredProducts.find((product) => product._id === productId);
-    console.log(product);
-    if (product) {
-        localStorage.setItem('product', JSON.stringify(product));
-        console.log('Product added to cart', product);
-    }
+  let product = filteredProducts.find((product) => product._id === productId);
+  console.log(product);
+  if (product) {
+    localStorage.setItem('product', JSON.stringify(product));
+    console.log('Product added to cart', product);
+  }
 }
 
 function initializeImageTitle() {
-    let imageUrl = 'assets/images/productShop/sup-head.jpeg';
-    let backgroundImage = 'url(' + imageUrl + ') center / cover';
+  let imageUrl = 'assets/images/productShop/sup-head.jpeg';
+  let backgroundImage = 'url(' + imageUrl + ') center / cover';
 
-    $('#titleImage').css('background', backgroundImage);
-    $("#titleH1").text('Discover, Shop, and Surf the Best');
+  $('#titleImage').css('background', backgroundImage);
+  $("#titleH1").text('Discover, Shop, and Surf the Best');
 }
 
 function btnOrganized() {
-    let isLogin = localStorage.getItem('user') !== null;
-    if (isLogin) {
-        $('#login').hide();
-        $('#logOut').show().click(function () {
-            localStorage.removeItem('user');
-            $('#login').show();
-            $('#logOut').hide();
-            $('#managerBtn').hide();
-            $('#clientBtn').hide();
-        });
-        if (JSON.parse(localStorage.getItem('user')).isAdmin) {
-            $('#managerBtn').show();
-            $('#clientBtn').hide();
-        } else {
-            $('#managerBtn').hide();
-            $('#clientBtn').show().click(function () {
-                window.location.href = 'personalAreaPage.html';
-            });
-        }
+  let isLogin = localStorage.getItem('user') !== null;
+  if (isLogin) {
+    $('#login').hide();
+    $('#logOut').show().click(function () {
+      localStorage.removeItem('user');
+      $('#login').show();
+      $('#logOut').hide();
+      $('#managerBtn').hide();
+      $('#clientBtn').hide();
+    });
+    if (JSON.parse(localStorage.getItem('user')).isAdmin) {
+      $('#managerBtn').show();
+      $('#clientBtn').hide();
     } else {
-        $('#logOut').hide();
-        $('#managerBtn').hide();
-        $('#clientBtn').hide();
+      $('#managerBtn').hide();
+      $('#clientBtn').show().click(function () {
+        window.location.href = 'personalAreaPage.html';
+      });
     }
+  } else {
+    $('#logOut').hide();
+    $('#managerBtn').hide();
+    $('#clientBtn').hide();
+  }
 }
 
 function loadFilters(field, filterList, menuList, filterName) {
-    $.ajax({
-        url: 'http://localhost:3000/products/groupBy/' + field,
-        type: 'GET',
-        dataType: 'json',
-        contentType: 'application/json',
-        success: function (data) {
-            console.log(data);
-            if (field === 'details.size') {
-                data.forEach(item => {
-                    item._id.forEach(size => {
-                        filterList.add(size)
-                    })
-                })
-            } else {
-                data.forEach(item => {
-                    filterList.add(item._id)
-                })
-            }
-            let value = filterName;
-            menuList.append(`<option value="${value}">${value}</option>`);
-            filterList.forEach((item) => {
-                menuList.append('<option value="' + item + '">' + item + '</option>');
-            });
-            filterProducts();
-        },
-        error: function (error) {
-            console.log(`Error ${error}`);
-            res = [];
-        }
-    })
+  $.ajax({
+    url: 'http://localhost:3000/products/groupBy/' + field,
+    type: 'GET',
+    dataType: 'json',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log(data);
+      if (field === 'details.size') {
+        data.forEach(item => {
+          item._id.forEach(size => {
+            filterList.add(size)
+          })
+        })
+      } else {
+        data.forEach(item => {
+          filterList.add(item._id)
+        })
+      }
+      let value = filterName;
+      menuList.append(`<option value="${value}">${value}</option>`);
+      filterList.forEach((item) => {
+        menuList.append('<option value="' + item + '">' + item + '</option>');
+      });
+      filterProducts();
+    },
+    error: function (error) {
+      console.log(`Error ${error}`);
+      res = [];
+    }
+  })
 }
 
 function filterProducts() {
-    let selectedCategory = CategoryList.val();
-    let selectedBrand = BrandList.val();
-    let selectedPrice = priceList.val();
-    let selectedSize = SizeList.val();
+  let selectedCategory = CategoryList.val();
+  let selectedBrand = BrandList.val();
+  let selectedPrice = priceList.val();
+  let selectedSize = SizeList.val();
 
-    $.ajax({
-        url: 'http://localhost:3000/products/filter',
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify(
-            {
-                category: selectedCategory,
-                brand: selectedBrand,
-                price: selectedPrice,
-                size: selectedSize
-            }),
-        success: function (products) {
-            $('#productContainer').empty();
+  $.ajax({
+    url: 'http://localhost:3000/products/filter',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify(
+      {
+        category: selectedCategory,
+        brand: selectedBrand,
+        price: selectedPrice,
+        size: selectedSize
+      }),
+    success: function (products) {
+      $('#productContainer').empty();
 
-            filteredProducts = products.filter((product) => {
-                return (
-                    (selectedCategory === 'All Categories' || product.category === selectedCategory) &&
-                    (selectedBrand === 'All Brands' || product.brand === selectedBrand) &&
-                    (selectedPrice === 'All Price' || (selectedPrice.includes('-') &&
-                            product.price >= parseFloat(selectedPrice.split('-')[0]) &&
-                            product.price <= parseFloat(selectedPrice.split('-')[1])) ||
-                        (selectedPrice === '200+' && product.price >= 200)) &&
-                    (selectedSize === 'All Sizes' || product.details.some(detail => detail.size === selectedSize))
-                );
-            });
+      filteredProducts = products.filter((product) => {
+        return (
+          (selectedCategory === 'All Categories' || product.category === selectedCategory) &&
+          (selectedBrand === 'All Brands' || product.brand === selectedBrand) &&
+          (selectedPrice === 'All Price' || (selectedPrice.includes('-') &&
+              product.price >= parseFloat(selectedPrice.split('-')[0]) &&
+              product.price <= parseFloat(selectedPrice.split('-')[1])) ||
+            (selectedPrice === '200+' && product.price >= 200)) &&
+          (selectedSize === 'All Sizes' || product.details.some(detail => detail.size === selectedSize))
+        );
+      });
 
+      if (filteredProducts.length === 0) {
+        $('#productContainer').append("<h1 class='h1-shop'> No products found </h1>");
+      } else {
+        for (let j = 0; j < filteredProducts.length; j++) {
+          let productId = filteredProducts[j]._id;
+          let productImage = filteredProducts[j].frontImage;
+          let productName = filteredProducts[j].name;
+          let productPrice = filteredProducts[j].price;
 
-            if (filteredProducts.length === 0) {
-                $('#productContainer').append("<h1> No products found </h1>");
-            } else {
-                for (let j = 0; j < filteredProducts.length; j++) {
-                    let productId = filteredProducts[j]._id;
-                    let productImage = filteredProducts[j].frontImage;
-                    let productName = filteredProducts[j].name;
-                    let productPrice = filteredProducts[j].price;
-
-                    let htmlCode = `
+          let htmlCode = `
                         <div id="productItem_${productId}" class="col">
                           <div>
                             <a id="product-cart_${productId}" href="./productPage.html" target="_blank">
